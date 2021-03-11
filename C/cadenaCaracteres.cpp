1f77420b2becb3088b8fcbd956f5a9f6cbcef49f
mcangrejo <74432387+mcangrejo@users.noreply.github.com>
2021-03-11 11:00:35 -0500

    Update cadenaCaracteres.cpp

--- C/cadenaCaracteres.cpp
+++ C/cadenaCaracteres.cpp
@@ -9,6 +9,8 @@ int main()
 	char c3='a';
 	char nombre[50]={0}; //se crea una cadena de caracteres con un tamano fijo y se llena de 0 o '\0' o NULL
 						//para poder capaturar del teclado cadenas de caracteres
+	char apellidos[50]={0};					
+						
 	char saludo[5]="Hola"; //creamos una cadena de caracteres. Se asigna un espacio adicional para el NULL
 							//que indica el fin de la cadena
 	printf("%s \n", saludo); //el especificador %s se usa para string(cadenas de caracteres)
@@ -19,7 +21,7 @@ int main()
 	printf("%s \n", curso);
 	printf("%c y %c \n", saludo[3], curso[1]); //el %c se usa para un solo caracter e imprime solo el cracter 
 									//que se especifica en el argumento
-	// printf("%s \n", saludo[0]); Esto no se debe hacer, bloquea al compilador. Espera una cadena de caracteres pero se le envia por parametro un solo caracter								
+							
 									
 	if(saludo[3]==curso[1])
 	{
@@ -29,6 +31,17 @@ int main()
 	printf("ingresa tu nombre:\n");
 	scanf("%s", nombre);  //para cadenas de caracteres no se usa el & de direccion, porque el nombre de la cadena
 						//ya es la direccion
-	printf("Hola %s", nombre)
+	getchar(); 
+	printf("Hola %s \n", nombre);
+	
+	
+	printf("ingresa tus apellidos:\n");
+	scanf("%[^\n]", apellidos);  //para ingresar palabras con espacio se usa %[^\n] tomara todo hast un enter
+	getchar();   //elimina el ultimo enter
+						
+						
+	printf("Hola %s %s", nombre, apellidos);
+	
+	
 	return 0;   	
 }
